3e21e18b37ec82dafb84c08657fe9ec5e7bc363b
meichflo <meichflo@students.zhaw.ch>
2024-04-10 20:01:21 +0200

    Fix KeyError in get_coordinates function

--- coordinates.py
+++ coordinates.py
@@ -10,13 +10,17 @@ def get_coordinates(address):
     """Takes an address as an argument.
     Returns the latitude and longitude of the address.
     Returns a tuple of floats in the mentioned order."""
+    print("Getting coordinates for: ", address)
     base_url= "https://api3.geo.admin.ch/rest/services/api/SearchServer?"
     parameters = {"searchText": address,
                 "origins": "address",
                 "type": "locations",
                 }
     r = requests.get(f"{base_url}{urllib.parse.urlencode(parameters)}")
-    data = json.loads(r.content)
-    Latitude = data['results'][0]['attrs']['lat']
-    Longitude = data['results'][0]['attrs']['lon']
+    try:
+        data = json.loads(r.content)
+        Latitude = data['results'][0]['attrs']['lat']
+        Longitude = data['results'][0]['attrs']['lon']
+    except (IndexError, KeyError):
+        return None, None
     return Latitude, Longitude  # return a tuple
