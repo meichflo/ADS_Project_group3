b155d993665a18bac6d4c1bd53589e0f7416af8f
meichflo <meichflo@students.zhaw.ch>
2024-04-16 19:33:34 +0200

    Refactor distance_to_station function for exception handling

--- distance_to_station.py
+++ distance_to_station.py
@@ -9,10 +9,10 @@ from math import radians, cos, sin, sqrt, atan2
 
 def get_distance_to_station(Latitude, Longitude):
     """
-    This function determines the closesd trainstation based on the input coordinates and returns the distance between the given Point and the nearest station.
+    This function determines the closesd trainstation based on the input coordinates and returns the distance between the given Point and the nearest station in km.
     NOTICE: Assumes that the input Points are in WGS84 projection (lat/lon).
     """
-    print("1. Calculating nearest station for coordinates", Latitude, Longitude)
+    # Get the town of the given coordinates
     url = "https://nominatim.openstreetmap.org/reverse"
     if Latitude is None or Longitude is None:
         return "No coordinates found"
@@ -31,13 +31,13 @@ def get_distance_to_station(Latitude, Longitude):
         Town = data['address']['city']
     else:
         return "No town found"
-    print("2. Found town", Town)
     url = "http://transport.opendata.ch/v1/locations"
     params = {
         'type': 'station',
         'query': Town
     }
-    # Get the coordinates of the nearest station
+
+    # Get the coordinates of the nearest station with exception handling
     try:
         data = requests.get(url, params=params).json()
         station_lat = data["stations"][0]["coordinate"]["x"]
@@ -47,7 +47,6 @@ def get_distance_to_station(Latitude, Longitude):
     # If no station is found, return "No station found"
     if station_lat is None or station_lon is None: 
         return "No station found"
-    print("Latitude:", type(Latitude), "Longitude:", type(Longitude), "station_lat:", type(station_lat), "station_lon:", type(station_lon))
 
     # Convert latitude and longitude to radians
     lat1, lon1, lat2, lon2 = map(radians, [Latitude, Longitude, station_lat, station_lon])
@@ -60,5 +59,4 @@ def get_distance_to_station(Latitude, Longitude):
     c = 2 * atan2(sqrt(a), sqrt(1-a))
     radius_of_earth_km = 6371.0
     distance = radius_of_earth_km * c
-    print("3. Calculated distance to station:", distance, "\n---------------------------------")
     return distance
